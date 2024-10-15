88e0f86a0f53ad7130669f7ee19ab4db8e5ded0f
wajid ansari <112863100+ansariwajid9999@users.noreply.github.com>
2024-10-15 20:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+const apiKey = "8f54b55aa094533082767f5822144bda"
+async function getWeather() {
+	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`
+	const response = await fetch(apiUrl)
+	if(!response.ok){
+	    throw new Error(error);
+	}
+	return response.json()
+}
+
+document.getElementById("myBtn").onclick = async () => {
+	const response = await getWeather()
+	document.getElementById("myP1").textContent = `Current weather in London: ${response.weather[0].description}`
+}
